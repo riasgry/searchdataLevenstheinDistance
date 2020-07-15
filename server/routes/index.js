b875e4e87b92d30a74b57c440533405acd2ab9d0
Luhur Hidayat Al-Rizki <44401575+riasgry@users.noreply.github.com>
2020-07-16 00:48:48 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -70,9 +70,10 @@ router.get('/autocomplete', async (req, res, next)=>{
 			for(var i=0; i<ngram.length; i++) {
 				var word = ngram[i].join(' ');
 				ngramsplit=word.split(',');
+				text_processing.push(ngramsplit)
 			
 			}
-			text_processing.push(splitter,ngramsplit) 
+			text_processing.push(splitter) 
 		});
 		function UnionSet(arr1,arr2){
 			return new Set([...arr1,...arr2])
